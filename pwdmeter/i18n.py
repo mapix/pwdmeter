4b09a6e595742308a830a4ab61cdce11f69d62d3
mapix <mapix.me@gmail.com>
2016-07-08 19:16:00 +0800

    remove unused

--- pwdmeter/i18n.py
+++ pwdmeter/i18n.py
@@ -4,7 +4,6 @@ from __future__ import unicode_literals
 import os
 import os.path
 import gettext
-from builtins import str
 
 resource_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'i18n')
 trans = gettext.translation('pwdmeter', localedir=resource_path, languages=[os.getenv('PWDMETER_GETTEXT_LANGUAGE', 'en')], fallback=True)
